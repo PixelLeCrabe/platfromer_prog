2229ed5f37642678ad1bb52ba970f2fb1f174850
lowenn <l.berthet@rubika-edu.com>
2021-09-20 09:07:14 +0200

    Add Attak Spe

--- Assets/assets/scripts/2D_plateformer/CaC_combat_systeme.cs
+++ Assets/assets/scripts/2D_plateformer/CaC_combat_systeme.cs
@@ -6,17 +6,24 @@ public class CaC_combat_systeme : MonoBehaviour
 {
     public Animator animator;
 
-    public float MCAttackDamage; 
+    public float MCAttackDamage;
+    public float MCattackRange = 0.2f;
     public Transform MCattackpoint;
-    public float MCattackRange = 0.5f;
-    public LayerMask EnemyLayerMask;
-    
     public float MCattackRate = 2f;
-    float nextAttack = 0f;
-   
+     private float nextAttack = 0f;
+    
+    public float MCSpecialAttakDammage;
+    public float MCSpecialAttakrange = 0.5f;
+    public Transform MCSpecialAttakpoint;
+    public float Specialattakrate =0.2f;
+    private float nextSpecialattak = 0f;
+
+    public LayerMask EnemyLayerMask;
+
+
     void Update()
     {
-        if(Time.time >= nextAttack)
+        if (Time.time >= nextAttack)
         {
             if (Input.GetKeyDown(KeyCode.T))
             {
@@ -28,22 +35,44 @@ public class CaC_combat_systeme : MonoBehaviour
         {
             animator.SetTrigger("Attack");
 
-           Collider2D[] hitEnnemy = Physics2D.OverlapCircleAll(MCattackpoint.position, MCattackRange,EnemyLayerMask) ;
-        
-            foreach(Collider2D enemy in hitEnnemy)
+            Collider2D[] hitEnnemy = Physics2D.OverlapCircleAll(MCattackpoint.position, MCattackRange, EnemyLayerMask);
+
+            foreach (Collider2D enemy in hitEnnemy)
             {
                 Debug.Log("enemy hit " + enemy.name);
 
                 enemy.GetComponent<Enemy>().takeDamage(MCAttackDamage);
             }
         }
-       
-    }
-    void OnDrawGizmoSelected()
-    {
-        Gizmos.DrawWireSphere(MCattackpoint.position, MCattackRange);
+
+        if (Time.time > nextSpecialattak)      
+        { 
+            if (Input.GetKeyDown(KeyCode.Y))
+            {
+                SpecialAttak();
+                nextSpecialattak = Time.time + 1F / Specialattakrate;
+                Debug.Log("Special atttatak");
+               }
+        }
+
+        void SpecialAttak()
+        {
+            animator.SetTrigger("SpecialAttak");
+            Collider2D[] hitEnnemy = Physics2D.OverlapCircleAll(MCSpecialAttakpoint.position, MCattackRange, EnemyLayerMask);
+
+            foreach (Collider2D enemy in hitEnnemy)
+            {
+                Debug.Log("enemy Special hit " + enemy.name);
+
+                enemy.GetComponent<Enemy>().takeDamage(MCSpecialAttakDammage);
+
+            }
+            void OnDrawGizmoSelected()
+            {
+                Gizmos.DrawWireSphere(MCattackpoint.position, MCattackRange);
+            }
+        }
     }
 }
-
 
 
