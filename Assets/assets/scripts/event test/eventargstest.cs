a0af64736bfc2d6496511673ec069a6f28e9ffd4
lowenn <l.berthet@rubika-edu.com>
2021-09-21 00:02:21 +0200

    event args

---
+++ Assets/assets/scripts/event test/eventargstest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eventargstest : MonoBehaviour
+{ 
+    public event EventHandler onspacepressed;
+    
+
+    void Start()
+    {
+       
+    }
+  
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            onspacepressed?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ Assets/assets/scripts/event test/eventargsuscriber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eventargsuscriber : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        eventargstest testingEvents = GetComponent<eventargstest>();
+        testingEvents.onspacepressed += TestingEvents_onspacepressed;
+    }
+
+    private void TestingEvents_onspacepressed(object sender, System.EventArgs e)
+    {
+        Debug.Log("aled");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
