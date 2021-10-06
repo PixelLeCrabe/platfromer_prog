c7d2551d9420f3782a935af037f26be12c2fdb3f
Krabe <l.berthet@rubika-edu.com>
2021-10-06 21:50:18 +0200

    g fait menage + script d'enchainement d'attak bugé

---
+++ Assets/CaC_animation_swap.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaC_animation_swap : MonoBehaviour
+{
+    public Animator animator;
+   
+    private bool CanSecondAttak;
+    private bool CanAttakk;
+    private bool firstBaseAttak;
+    private float Attakinput;
+
+    public float SecondAttaktimer;
+    public float baseattakCD;
+
+    private int nbofAttakIpunt;
+    
+    void Start()
+    {
+        nbofAttakIpunt = 0;
+        //CanSecondAttak = true;
+        CanAttakk = true;
+        //CanSecondAttak = true;
+        animator.SetBool("CantfirstAttak", false);
+    }
+
+    private void BaseAttak()
+    {
+        if (CanAttakk)
+        {
+            if ((Input.GetKeyDown(KeyCode.T) || Input.GetButtonDown("Fire2")) && !CanSecondAttak)
+            {
+                CanAttakk = false;
+                //base Attak CD
+                animator.SetBool("CantfirstAttak", true);
+                StartCoroutine(coroutineBaseAttakCD());
+                //retun to base attack if there is only on input after a certain aùpount of time 
+                StartCoroutine(coroutineSecondAttakTimer());
+                animator.SetTrigger("baseAttack1");
+                Debug.Log("base attak 1");
+                //Wait for 2nd Attak
+                StartCoroutine(coroutineCanSecondAttak());
+            }
+        }
+
+        if ((Input.GetKeyDown(KeyCode.T) || Input.GetButtonDown("Fire2")) && CanSecondAttak)
+        {
+            CanAttakk = false;
+            print("Second attak");
+            //base Attak CD
+            animator.SetBool("CantfirstAttak", false);
+            animator.SetTrigger("baseAttack2");
+            StartCoroutine(coroutineCanSecondAttak());
+            StartCoroutine(coroutineBaseAttakCD());
+        }
+    }
+    
+    private void Attackinputcount()
+    {
+        if (Input.GetButtonDown("Fire2") || Input.GetKeyDown(KeyCode.T))
+        {
+            nbofAttakIpunt++;
+        }
+    }
+
+    private void canSecondAttakCheck()
+    {
+        if (nbofAttakIpunt > 1)
+        {
+            CanSecondAttak = true;
+        }
+    }
+    void Update()
+    {
+        Attackinputcount();
+        canSecondAttakCheck();
+        BaseAttak();
+    }
+
+    IEnumerator coroutineSecondAttakTimer()
+    {
+        yield return new WaitForSeconds(SecondAttaktimer);
+        CanSecondAttak = false;
+
+    }
+
+    IEnumerator coroutineBaseAttakCD()
+    {
+        yield return new WaitForSeconds(baseattakCD);
+        CanAttakk = true;
+    }
+
+    IEnumerator coroutineCanSecondAttak()
+    {
+        yield return new WaitForSeconds(.2f);
+        animator.SetBool("CantfirstAttak", false);
+        CanSecondAttak = false;
+        nbofAttakIpunt = 0;
+    } 
+}
---
+++ Assets/Cac_swapv2.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cac_swapv2 : MonoBehaviour
+{
+    public Animator animator;
+
+    private bool CanSecondAttak;
+    private bool CanAttakk;
+    private bool firstBaseAttak;
+    private float Attakinput;
+
+    public float baseattakCD;
+
+    void Start()
+    {
+        //CanSecondAttak = true;
+        CanAttakk = true;
+        CanSecondAttak = false;
+        //CanSecondAttak = true;
+    }
+
+    private void BaseAttak()
+    {
+        if (CanAttakk)
+        {
+            if ((Input.GetKeyDown(KeyCode.T) || Input.GetButtonDown("Fire2")) && !CanSecondAttak)
+            {
+                CanAttakk = false;
+                //CanSecondAttak = true;
+                animator.SetTrigger("baseAttack1");
+                Debug.Log("base attak 1");
+                StartCoroutine(coroutineCanSecondAttak());
+                StartCoroutine(coroutineCanTSecondAttak());
+                if ( CanSecondAttak)
+                    {
+                        
+                    }
+
+                if ((Input.GetKeyDown(KeyCode.T) || Input.GetButtonDown("Fire2")) && CanSecondAttak)
+                {
+                    CanAttakk = false;
+                    print("Second attak");
+                    animator.SetTrigger("baseAttack2");
+                    StartCoroutine(coroutineBaseAttakCD());
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        BaseAttak();
+    }
+
+    IEnumerator coroutineBaseAttakCD()
+    {
+        yield return new WaitForSeconds(baseattakCD);
+        CanAttakk = true;
+        CanSecondAttak = false;
+    }
+
+    IEnumerator coroutineCanSecondAttak()
+    {
+        yield return new WaitForSeconds(.2f);
+        animator.SetBool("CantfirstAttak", false);
+        CanSecondAttak = true;
+    }
+
+    IEnumerator coroutineCanTSecondAttak()
+    {
+        yield return new WaitForSeconds(.3f);
+        CanSecondAttak = false;
+    }
+}
+
